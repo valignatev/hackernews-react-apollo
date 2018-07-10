198aa287e1e4c4928cc938917c93dd0d95c04d1b
Valentin Ignatev <valentignatev@gmail.com>
2018-07-10 18:58:32 +1000

    chapter 6

--- src/index.js
+++ src/index.js
@@ -6,12 +6,25 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-client-preset';
 import { HttpLink } from 'apollo-link-http';
 import {InMemoryCache } from 'apollo-cache-inmemory';
+import { AUTH_TOKEN } from './constants';
 
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  const authorezationHeader = token ? `Bearer ${token}` : null;
+  operation.setContext({
+    headers: {
+      authorization: authorezationHeader,
+    }
+  });
+  return forward(operation);
+});
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink);
 const client = new ApolloClient({
-  link: httpLink,
+  link: httpLinkWithAuthToken,
   cache: new InMemoryCache(),
 });
 

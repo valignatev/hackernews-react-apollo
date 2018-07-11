df21ef3ef92c0fdffabdc05b7e2b0144d1f3781e
Valentin Ignatev <valentignatev@gmail.com>
2018-07-11 12:33:44 +1000

    chapter 7

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import CreateLink from './CreateLink';
 import LinkList from './LinkList';
 import Header from './Header';
 import Login from './Login';
+import Search from './Search';
 
 class App extends Component {
   render() {
@@ -12,9 +13,10 @@ class App extends Component {
         <Header />
         <div className="ph3 pv1 background-gray">
           <Switch>
+            <Route exact path="/" component={LinkList} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/create" component={CreateLink} />
-            <Route exact path="/" component={LinkList} />
+            <Route exact path="/search" component={Search} />
           </Switch>
         </div>
       </div>
